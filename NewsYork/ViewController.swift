dafe142a0bf4e316369cc186e1d91f9a3d7c88da
beyzanurtekerek <beyzanrtkrk@gmail.com>
2025-02-20 20:22:13 +0300

    Initial Commit

---
+++ NewsYork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsYork
+//
+//  Created by Beyza Nur Tekerek on 20.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
