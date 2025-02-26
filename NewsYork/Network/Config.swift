764eb7c9f507e7b66d21f5fd4de8383b5f5ca005
beyzanurtekerek <beyzanrtkrk@gmail.com>
2025-02-26 17:24:55 +0300

    Add apiKey configuration in Config.swift for API integration

--- NewsYork/Network/Config.swift
+++ NewsYork/Network/Config.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct Config {
+    static let apiKey: String = {
+        guard let path = Bundle.main.path(forResource: "Config", ofType: "plist"),
+              let config = NSDictionary(contentsOfFile: path),
+              let apiKey = config["NYT_API_KEY"] as? String else {
+            fatalError("Config.plist dosyası veya NYT_API_KEY bulunamadı!")
+        }
+        return apiKey
+    }()
+}
