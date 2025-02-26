b6781448c1ab4c753fccc7b14d9b1a7ae89fc54d
beyzanurtekerek <beyzanrtkrk@gmail.com>
2025-02-26 17:08:40 +0300

    .gitignore created

---
+++ NewsYork/Network/Config.swift
@@ -0,0 +1,8 @@
+//
+//  Config.swift
+//  NewsYork
+//
+//  Created by Beyza Nur Tekerek on 26.02.2025.
+//
+
+import Foundation
---
+++ NewsYork/Network/NewsService.swift
@@ -0,0 +1,8 @@
+//
+//  NewsService.swift
+//  NewsYork
+//
+//  Created by Beyza Nur Tekerek on 26.02.2025.
+//
+
+import Foundation
